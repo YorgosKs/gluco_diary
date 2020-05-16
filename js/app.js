274907615a2713745c2ebb31026b4543feee55a6
geoks <gkoutsikos1@gmail.com>
2020-05-16 23:01:14 +0200

    gluco diary beta

---
+++ js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+(function (){
+	function init(){
+		var router= new Router([
+			new Route('home', '/home.html', true),
+			new Route('food', '/foodform.html'),
+			new Route('measurement', '/measurement.html')
+			]);
+
+
+	  
+	}
+
+
+
+
+	
+	init();
+}());
